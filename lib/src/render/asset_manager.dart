cbfa416ea189550f67af1caa7c9c9a30adb60c48
Farhad <fardzia@gmail.com>
2024-08-25 15:09:15 +0000

    Update package

---
+++ lib/src/render/asset_manager.dart
@@ -0,0 +1,165 @@
+import 'dart:convert';
+
+import 'package:webapp/src/render/web_request.dart';
+import 'package:webapp/src/tools/console.dart';
+import 'package:path/path.dart' as p;
+import 'package:webapp/wa_server.dart';
+
+class AssetManager {
+  List<Asset> includes = [];
+  Map<String, Object?> data = {};
+
+  AssetManager(WebRequest rq) {
+    includes.insert(
+      0,
+      Asset(
+        rq: rq,
+        path: '/app/includes.js',
+        cache: AssetCache.never,
+      ),
+    );
+  }
+
+  String getDataJs() {
+    try {
+      var allData = {};
+      for (var asset in js) {
+        allData.addAll(asset.data);
+      }
+
+      allData.addAll(data);
+      var json = jsonEncode(allData);
+      return '<script type="text/javascript">;var request = $json;</script>';
+    } catch (e) {
+      Console.e(e);
+    }
+
+    return '';
+  }
+
+  List<Asset> get js {
+    return getByType(AssetType.js);
+  }
+
+  List<Asset> get css {
+    return getByType(AssetType.css);
+  }
+
+  List<Asset> getByType(AssetType type) {
+    return includes.where((asset) => asset.type == type).toList();
+  }
+
+  AssetManager addAssets(List<Asset> assets) {
+    includes.addAll(assets);
+    return this;
+  }
+
+  AssetManager addAsset(Asset asset) {
+    includes.add(asset);
+    return this;
+  }
+
+  AssetManager addData(Map<String, Object?> params) {
+    data.addAll(params);
+    return this;
+  }
+
+  AssetManager addNotify({required String text, String type = 'success'}) {
+    var notify = (data['notify'] ?? []) as List;
+    notify.add({
+      'text': text,
+      'type': type,
+    });
+    data['notify'] = notify;
+    return this;
+  }
+
+  String getCssUI() {
+    List<String> result = [];
+
+    for (var c in css) {
+      result.add('<link rel="stylesheet" href="${c.url}" />');
+    }
+
+    return result.join('\n');
+  }
+
+  String getJsUI() {
+    List<String> result = [];
+
+    for (var j in js) {
+      String attrs = '';
+      j.attrs.forEach((key, value) {
+        attrs = '$attrs $key="$value"';
+      });
+      result.add('<script $attrs src="${j.url}"></script>');
+    }
+
+    return result.join('\n');
+  }
+}
+
+class Asset {
+  late String _path;
+  WebRequest rq;
+  AssetType type;
+  AssetCache cache = AssetCache.appVersion;
+  Map<String, Object> data;
+  Map<String, Object> attrs;
+
+  Asset({
+    required this.rq,
+    required path,
+    this.type = AssetType.none,
+    this.cache = AssetCache.appVersion,
+    this.data = const {},
+    this.attrs = const {},
+  }) {
+    _path = path;
+    if (type == AssetType.none) {
+      final extention = p.extension(path).toLowerCase();
+      switch (extention) {
+        case '.js':
+          type = AssetType.js;
+          break;
+        case '.css':
+          type = AssetType.css;
+          break;
+      }
+    }
+  }
+
+  Asset addData(Map<String, String> params) {
+    data.addAll(params);
+    return this;
+  }
+
+  String get url {
+    if (cache == AssetCache.never) {
+      return rq.url(
+        _path,
+        params: {
+          'vr': "v1.${DateTime.now().millisecondsSinceEpoch}",
+        },
+      );
+    } else if (cache == AssetCache.appVersion) {
+      return rq.url(
+        _path,
+        params: {
+          'vr': "v${WaServer.config.version}",
+        },
+      );
+    } else {
+      return rq.url(_path);
+    }
+  }
+
+  @override
+  String toString() {
+    return url;
+  }
+}
+
+enum AssetType { css, js, none }
+
+enum AssetCache { never, cache, appVersion }
---
+++ lib/src/views/ui_paging.dart
@@ -0,0 +1,120 @@
+import 'package:webapp/wa_route.dart';
+import 'package:webapp/wa_tools.dart';
+import 'package:webapp/src/views/wa_views.dart';
+
+class UIPaging extends WaView {
+  int page;
+  int total;
+  int pageSize;
+  int widthSide;
+  String profix;
+  Map<String, String> otherQuery;
+  bool useRequsetQueries;
+  String orderBy;
+  bool orderReverse;
+
+  int get start {
+    var start = (page - 1) * pageSize;
+    if (start >= total) {
+      start =
+          total % pageSize == 0 ? total - pageSize : total - (total % pageSize);
+    }
+    return start;
+  }
+
+  UIPaging({
+    required super.rq,
+    required super.widget,
+    super.params,
+
+    ///
+    required this.page,
+    required this.total,
+    this.pageSize = 20,
+    this.profix = 'page',
+    this.otherQuery = const <String, String>{},
+    this.widthSide = 2,
+    this.useRequsetQueries = false,
+
+    ///
+    this.orderBy = '',
+    this.orderReverse = false,
+  });
+
+  factory UIPaging.fromRequest(
+    WebRequest rq, {
+    required int total,
+    String profix = 'page',
+    String widget = 'dashboard/theme/ui/paging',
+    Map<String, String> otherQuery = const {},
+    int pageSize = 5,
+    String orderByDef = '',
+    bool orderReverseDef = false,
+    int pageDef = 1,
+  }) {
+    var page = rq.get<int>(profix, def: pageDef);
+    var pageSizeFix = rq.get<int>('pageSize', def: pageSize);
+    var orderBy = rq.get<String>('orderBy', def: orderByDef);
+    var orderReverse = rq.get<bool>('orderReverse', def: orderReverseDef);
+
+    return UIPaging(
+      rq: rq,
+      widget: widget,
+      page: page,
+      total: total,
+      pageSize: pageSizeFix,
+      orderBy: orderBy,
+      orderReverse: orderReverse,
+      profix: profix,
+      otherQuery: otherQuery,
+    );
+  }
+
+  @override
+  Future<Map<String, Object?>> renderData() async {
+    page = page < 1 ? 1 : page;
+    var count = (total / pageSize).ceil();
+    if (page > count) {
+      page = count;
+    }
+    var toEnd = page * pageSize;
+    toEnd = (toEnd > total) ? total : toEnd;
+    var rangeFrom = page - widthSide;
+    var rangeTo = page + widthSide;
+    if (page <= widthSide) {
+      rangeTo += widthSide - page + 1;
+    }
+
+    if (count - widthSide < page) {
+      rangeFrom -= widthSide - (count - page);
+    }
+
+    if (useRequsetQueries) {
+      rq.uri.queryParameters.forEach((key, value) {
+        if (key != profix && !otherQuery.containsKey(key)) {
+          otherQuery[key] = value;
+        }
+      });
+    }
+
+    var other = otherQuery.joinMap('=', '&');
+
+    var viewParams = {
+      'total': total,
+      'count': count,
+      'page': page,
+      'pageSize': pageSize,
+      'toEnd': toEnd,
+      'profix': profix,
+      'rangeTo': rangeTo,
+      'rangeFrom': rangeFrom,
+      'disableFirst': page - 1 <= 0,
+      'disableLast': page - count >= 0,
+      'other': other.isEmpty ? "" : "&$other",
+      'orderBy': orderBy,
+      'orderReverse': orderReverse,
+    };
+
+    return viewParams;
+  }
+}

ea3e1bc82d8912283312dd045ef0fea357385d92
Farhad <fardzia@gmail.com>
2024-08-26 21:46:45 +0000

    Update package

---
+++ lib/src/crons/wa_cron.dart
@@ -0,0 +1,132 @@
+import 'package:cron/cron.dart';
+
+/// A class to manage and run cron jobs using the `cron` package.
+///
+/// The [WaCron] class schedules and manages tasks based on the given cron schedule.
+/// It includes functionalities for delayed starts, counting executions, and tracking status.
+///
+/// Example:
+/// ```dart
+/// final cronJob = WaCron(
+///   schedule: '*/5 * * * * *', // Run every 5 seconds
+///   onCorn: (count, cron) async {
+///     print('Task executed $count times');
+///   },
+/// );
+///
+/// cronJob.start();
+/// ```
+class WaCron {
+  /// Cron schedule in string format.
+  ///
+  /// The schedule should be in a cron format (e.g., `*/5 * * * * *`).
+  String schedule;
+
+  /// Internal instance of the Cron package.
+  final _cron = Cron();
+
+  /// The time the cron was registered (in microseconds since epoch).
+  ///
+  /// This is automatically set when the [WaCron] instance is created.
+  late final int registerTime;
+
+  /// The callback function to be executed on each cron tick.
+  ///
+  /// The function receives the current tick count and the [WaCron] instance as parameters.
+  Future Function(int count, WaCron cron) onCorn;
+
+  /// The number of times the cron task has been executed.
+  int get counter => _counter;
+
+  /// Internal counter to track the number of executions.
+  int _counter = 0;
+
+  /// Indicates whether the first execution should be delayed.
+  ///
+  /// If set to `true`, the first tick will not run immediately when the cron starts.
+  bool delayFirstMoment = true;
+
+  /// The current status of the cron job.
+  ///
+  /// Can be [CronStatus.notStarted], [CronStatus.running], or [CronStatus.stoped].
+  CronStatus _status = CronStatus.notStarted;
+
+  /// The current status of the cron job.
+  ///
+  /// Can be [CronStatus.notStarted], [CronStatus.running], or [CronStatus.stoped].
+  /// Public getter to access the current cron status.
+  CronStatus get status => _status;
+
+  /// Creates an instance of [WaCron].
+  ///
+  /// [onCorn] is required and specifies the callback function to be executed on each cron tick.
+  /// [schedule] is required and specifies the cron schedule.
+  /// [delayFirstMoment] is optional and defaults to `true`. When `false`, the first tick runs immediately.
+  WaCron({
+    required this.onCorn,
+    required this.schedule,
+    this.delayFirstMoment = true,
+  }) {
+    registerTime = DateTime.now().microsecondsSinceEpoch;
+  }
+
+  /// Starts the cron job.
+  ///
+  /// This method changes the cron status to [CronStatus.running] and schedules the tasks according to the [schedule].
+  /// If [delayFirstMoment] is `false`, the first tick runs immediately.
+  /// Returns the instance of [WaCron] for chaining.
+  WaCron start() {
+    _status = CronStatus.running;
+
+    if (delayFirstMoment == false) {
+      _counter++;
+      onCorn(_counter, this);
+    }
+
+    _cron.schedule(Schedule.parse(schedule), () async {
+      _counter++;
+      onCorn(_counter, this);
+    });
+
+    return this;
+  }
+
+  /// Converts a [Duration] to a cron expression.
+  ///
+  /// This method is useful if you want to generate a cron schedule from a [Duration].
+  /// Throws an [ArgumentError] if the duration is less than 1 second.
+  ///
+  /// Example:
+  /// ```dart
+  /// String cronExpr = WaCron.durationToCron(Duration(seconds: 10)); // "*/10 * * * * *"
+  /// ```
+  static String durationToCron(Duration duration) {
+    int sec = duration.inSeconds;
+    if (sec < 1) {
+      throw ArgumentError('Duration must be at least 1 sec.');
+    }
+    String cronExpression = '*/$sec * * * * *';
+
+    return cronExpression;
+  }
+
+  /// Stops the cron job and cleans up resources.
+  ///
+  /// This method changes the cron status to [CronStatus.stoped] and closes the internal [Cron] instance.
+  void close() {
+    _cron.close();
+    _status = CronStatus.stoped;
+  }
+}
+
+/// Enum to represent the status of the cron job.
+enum CronStatus {
+  /// Indicates that the cron job is currently running.
+  running,
+
+  /// Indicates that the cron job has been stopped.
+  stoped,
+
+  /// Indicates that the cron job has not started yet.
+  notStarted,
+}

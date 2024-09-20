b639df5240e0a3a25df6a2793610093ca46dd51b
Farhad <fardzia@gmail.com>
2024-09-20 20:56:59 +0000

    New release 1.0.26

--- lib/src/wa_server.dart
+++ lib/src/wa_server.dart
@@ -231,5 +231,5 @@ class WaServer {
 /// A class that holds version information for the server.
 class _Info {
   /// The version of the server.
-  final String version = '1.0.0';
+  final String version = '1.0.26';
 }

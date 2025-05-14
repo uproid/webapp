0ee392b7489663a7b6794c5bb31413e7b54ac47b
Uproid <fardzia@gmail.com>
2025-05-14 16:27:05 +0200

    Reapply "New release 1.1.18"
    This reverts commit a130abb0999170bc968959ed2b0a6f5e5817434b.

--- lib/src/wa_server.dart
+++ lib/src/wa_server.dart
@@ -248,5 +248,5 @@ class WaServer {
 /// A class that holds version information for the server.
 class _Info {
   /// The version of the server.
-  final String version = '1.1.17';
+  final String version = '1.1.18';
 }

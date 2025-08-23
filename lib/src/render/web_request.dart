539b8ab40081975cea9466f70f53542c88ef2888
Farhad <farhad@toomba.nl>
2025-08-23 03:35:30 +0200

    Fixed

--- lib/src/render/web_request.dart
+++ lib/src/render/web_request.dart
@@ -1380,8 +1380,11 @@ class WebRequest {
     }
 
     /// When the request is HTTPS, the URL should be HTTPS as well.
+    var port = _rq.uri.port;
     uri = uri.replace(scheme: uri.scheme);
-
+    if (port != 80 && port != 443) {
+      uri = uri.replace(port: port);
+    }
     var url = uri.toString();
     return url;
   }

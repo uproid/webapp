80f597c8b19b6487a663da3ae508edbe54b61722
Farhad <farhad@toomba.nl>
2025-10-04 00:29:39 +0200

    New version v2.1.1

--- lib/src/wa_server.dart
+++ lib/src/wa_server.dart
@@ -725,5 +725,5 @@ class WaServer {
 /// A class that holds version information for the server.
 class _Info {
   /// The version of the server.
-  final String version = '2.1.0';
+  final String version = '2.1.1';
 }
